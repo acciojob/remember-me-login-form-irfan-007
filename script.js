b35009771b761f6bb88653517b3ad7a47b96ba79
irfan <85334710+irfan-007@users.noreply.github.com>
2024-09-02 17:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const submit = document.getElementById("submit");
+const existing = document.getElementById("existing");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+if (localStorage.getItem("name") && localStorage.getItem("password"))
+  existing.style.display = "block";
+else existing.style.display = "none";
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let name = username.value;
+  let pass = password.value;
+  if (checkbox.checked) {
+    localStorage.setItem("name", name);
+    localStorage.setItem("password", pass);
+  } else {
+    localStorage.removeItem("name");
+    localStorage.removeItem("password");
+  }
+  alert("Logged in as");
+});
+
+existing.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("Logged in as");
+});
